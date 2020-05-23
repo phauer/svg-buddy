68cf107d9a880f212c3f908778144ceb19d2e8bb
Philipp Hauer <info@philipphauer.de>
2020-05-23 17:36:08 +0200

    build: separate build and local install

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+./mvnw clean
 ./mvnw package -Dquarkus.package.uber-jar=true
 ./mvnw package -Pnative -Dquarkus.native.container-build=true
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-./mvnw package -Pnative -Dquarkus.native.container-build=true
 mkdir -p  ~/bin
 rm -f ~/bin/svg-font-embedding
-cp target/svg-font-embedding-runner target/svg-font-embedding
 cp target/svg-font-embedding-runner ~/bin/svg-font-embedding
